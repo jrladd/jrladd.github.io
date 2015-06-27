7ad8fd61d7b247ae9ff30c0fedb40ebd1fdfc38b
JR Ladd <johnrobertladd@gmail.com>
2015-06-27 15:27:44 -0500

    fix graph

--- networks/updated/1639-1648/1639-1648.js
+++ networks/updated/1639-1648/1639-1648.js
@@ -44,9 +44,8 @@ d3.json("1639_1648.json", function(error, graph) {
 
   var link = vis.selectAll(".link")
       .data(graph.links)
-      .enter().append("line")
+    .enter().append("line")
       .attr("class", "link")
-      .style('stroke', '#000')
       .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
       
@@ -65,24 +64,18 @@ d3.json("1639_1648.json", function(error, graph) {
        // http://stackoverflow.com/a/19125306
 		  // On node hover, examine the links to see if their
 		  // source or target properties match the hovered node.
-		  node.on('mouseover', function() {
-		    d=d3.select(this).node().__data__;
-		    node.style('opacity', function (o) {
-		      return neighboring(d, o) | neighboring (o, d) ? 1 : 0.1;
-		    });
-//		    link.style('stroke', function(l) {
-//		      if (d === l.source || d === l.target)
-//		        return "#0000FF";
-//		      else
-//		        return "#d3d3d3";
-             
+		  node.on('mouseover', function(d) {
+		    link.style('stroke', function(l) {
+		      if (d === l.source || d === l.target)
+		        return "#0000FF";
+		      else
+		        return "#fff";
 		      });
 		  });
 
 		  // Set the stroke width back to normal when mouse leaves the node.
 		  node.on('mouseout', function() {
-		    node.style('opacity', 1);
-		    link.style('stroke', "#000");
+		    link.style('stroke', "#999");
 		  });
 
   
