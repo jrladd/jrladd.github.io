870d9325918c2f3d84a32f5ef7f1ca78fef59ca1
JR Ladd <johnrobertladd@gmail.com>
2015-06-27 15:31:24 -0500

    try opacity

--- networks/updated/1639-1648/1639-1648.js
+++ networks/updated/1639-1648/1639-1648.js
@@ -64,12 +64,15 @@ d3.json("1639_1648.json", function(error, graph) {
        // http://stackoverflow.com/a/19125306
 		  // On node hover, examine the links to see if their
 		  // source or target properties match the hovered node.
-		  node.on('mouseover', function(d) {
-		    link.style('stroke', function(l) {
-		      if (d === l.source || d === l.target)
-		        return "#0000FF";
-		      else
-		        return "#fff";
+		  node.on('mouseover', function() {
+		    d=d3.select(this).node().__data__;
+		    node.style('opacity', function (o) {
+		      return neighboring(d, o) | neighboring (o, d) ? 1 : 0.1;
+//		    link.style('stroke', function(l) {
+//		      if (d === l.source || d === l.target)
+//		        return "#0000FF";
+//		      else
+//		        return "#fff";
 		      });
 		  });
 
